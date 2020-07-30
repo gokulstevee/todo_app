21adf10bd38d68db54ea0ca7db9f602af889f99e
gokulstevee <65470058+gokulstevee@users.noreply.github.com>
2020-07-31 02:50:00 +0530

    add controls added

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var ul = document.getElementById('list')
 var li;
+var input = document.getElementById('input')
+var text = input.value
 
 var a = document.getElementById('add')
 a.addEventListener('click', addItem)
 
 var b = document.getElementById('remove')
 b.addEventListener('click', removeItem)
-
-
 
 function addItem() {
   console.log("added");
@@ -24,20 +24,44 @@ function removeItem() {
 }
 
 function addItem() {
-  let input = document.getElementById('input')
-  let text = input.value
   let p = document.querySelector('#p1')
   let pchildren = p.children
-  if (text === '' && pchildren.length===0) {
+  if (input.value === '' && pchildren.length === 0) {
     let p = document.getElementById('p1')
     let createp = document.createElement('p')
     createp.textContent = '*Enter Your Todo to add'
     p.appendChild(createp)
   }
-  else if (text!=='') {
+  else if (input.value !== '') {
+
+    // adding todo
+    var textnode = document.createTextNode(input.value)
+    // create li 
+    li = document.createElement('li')
+
+    // create checkbox 
+    var checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.setAttribute('id', 'check')
+
+    // create label
+    var label = document.createElement('label')
+    label.setAttribute('for', 'item')//optional
+
+    // appending
+    label.appendChild(textnode)
+    li.appendChild(checkbox)
+    li.appendChild(label)
+    ul.appendChild(li)
+    // ul.insertBefore(li, ul.childNodes[0])
+    setTimeout(() => {
+      li.className = 'visual'
+    }, 2)
+    input.value = ''
+
     for (let index = 0; index < pchildren.length; index++) {
       const element = pchildren[index];
       p.removeChild(element)
     }
-  } 
+  }
 }
